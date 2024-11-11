080431ec0927a9795f47aae58df16e7e68ca3d88
mazazyrik <mazazyrik@yandex.ru>
2024-11-11 21:30:21 +0300

    some bugs

--- app/main.py
+++ app/main.py
@@ -1,11 +1,47 @@
 # flake8: noqa
 import flet as ft
 import time
-from pathlib import Path
 from style import colors
-from test import test_screen
+from config import BASE_DIR
+from test import list_view_content
 
-BASE_DIR = Path(__file__).parent.resolve()
+def create_card(title, description, image_src):
+    return ft.Card(
+        content=ft.Row(
+            controls=[
+                ft.Container(
+                    content=ft.Column(
+                        controls=[
+                            ft.Text(title, size=15, weight="bold",
+                                    color=colors['Dark']["text"], text_align=ft.TextAlign.LEFT),
+                            ft.Text(
+                                description,
+                                color=colors['Dark']["text"], text_align=ft.TextAlign.LEFT, size=13
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.START,
+                        spacing=5,
+                    ),
+                    padding=10,
+                ),
+                ft.Container(
+                    content=ft.Image(
+                        src=image_src,
+                        fit=ft.ImageFit.COVER
+                    ),
+                    width=175,
+                    height=100,
+                    padding=10,
+                    clip_behavior=ft.ClipBehavior.ANTI_ALIAS
+                )
+            ],
+            alignment=ft.MainAxisAlignment.START,
+            spacing=10,
+        ),
+        elevation=5,
+        height=150,
+        animate_size=True,
+    )
 
 
 def main(page: ft.Page):
@@ -46,46 +82,39 @@ def main(page: ft.Page):
                     color=theme["text"],
                     text_align=ft.TextAlign.CENTER
                 ),
-
-                ft.Card(
-                    content=ft.Row(
-                        controls=[
-                            ft.Container(
-                                content=ft.Column(
-                                    controls=[
-                                        ft.Text("Т‑Банк Финтех", size=15, weight="bold",
-                                                color=theme["text"], text_align=ft.TextAlign.LEFT),
-                                        ft.Text(
-                                            "Образовательные ИТ-курсы от топ-менеджеров, техлидов и ведущих специалистов Т‑Банка",
-                                            color=theme["text"], text_align=ft.TextAlign.LEFT, size=13
-                                        ),
-                                    ],
-                                    alignment=ft.MainAxisAlignment.START,
-                                    spacing=10,
-                                    width=175,
-                                ),
-                                padding=10,
-                            ),
-                            ft.Container(
-                                content=ft.Image(
-                                    src=f"{BASE_DIR}/static/1.webp",
-                                    fit=ft.ImageFit.COVER
-                                ),
-                                width=175,
-                                height=100,
-                                padding=10,
-                                clip_behavior=ft.ClipBehavior.ANTI_ALIAS
-                            )
-                        ],
-                        alignment=ft.MainAxisAlignment.START,
-                        spacing=10,
-                    ),
-                    elevation=5,
-                    width=375,
-                    height=150,
-                    animate_size=True,
+                ft.ListView(
+                    controls=[
+                        create_card(
+                            "Т‑Банк Финтех",
+                            "Образовательные ИТ-курсы от топ-менеджеров, техлидов и ведущих специалистов Т‑Банка",
+                            f"{BASE_DIR}/static/1.webp"
+                        ),
+                     ],
+                    spacing=10,  
+                    padding=10,  
                 )
+            ],
+            alignment=ft.MainAxisAlignment.START,
+            spacing=20,
+        ),
+        bgcolor=theme["background"],
+        height=page.window.height - 130,
+    )
 
+    test_screen = ft.Container(
+        content=ft.Column(
+            controls=[
+                ft.Row(
+                    controls=[ft.Text(
+                        "Тестовый экран", size=24, weight=ft.FontWeight.BOLD, color=theme["text"])],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    spacing=10
+                ),
+                ft.Text(
+                    "Здесь может быть ваш контент для тестового экрана.", color=theme["text"]),
+                ft.Image(
+                    src=f"{BASE_DIR}/static/bigboss.jpg",
+                    width=550, height=500),
             ],
             alignment=ft.MainAxisAlignment.START,
             spacing=10,
@@ -94,32 +123,11 @@ def main(page: ft.Page):
         height=page.window.height - 130
     )
 
-    # test_screen = ft.Container(
-    #     content=ft.Column(
-    #         controls=[
-    #             ft.Row(
-    #                 controls=[ft.Text(
-    #                     "Тестовый экран", size=24, weight=ft.FontWeight.BOLD, color=theme["text"])],
-    #                 alignment=ft.MainAxisAlignment.CENTER,
-    #                 spacing=10
-    #             ),
-    #             ft.Text(
-    #                 "Здесь может быть ваш контент для тестового экрана.", color=theme["text"]),
-    #             ft.Image(
-    #                 src=f"{BASE_DIR}/static/bigboss.jpg",
-    #                 width=550, height=500),
-    #         ],
-    #         alignment=ft.MainAxisAlignment.START,
-    #         spacing=10,
-    #     ),
-    #     bgcolor=theme["background"],
-    #     height=page.window.height - 130
-    # )
-
     def show_screen(screen):
         page.controls.clear()
         page.add(screen)
         page.add(bottom_nav)
+        page.add(list_view_content)
         page.update()
 
         for i in range(0, 101, 5):
@@ -132,6 +140,8 @@ def main(page: ft.Page):
             show_screen(main_screen)
         elif selected_index == 1:
             show_screen(test_screen)
+        elif selected_index == 2:
+            show_screen(list_view_content)
 
     bottom_nav = ft.Container(
         content=ft.Row(
@@ -141,6 +151,16 @@ def main(page: ft.Page):
                         ft.IconButton(
                             ft.icons.HOME, on_click=lambda e: on_navigation_change(0), icon_size=24),
                         ft.Text("Главная", size=12,
+                                color=colors["Dark"]["text"]),
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                ),
+                ft.Column(
+                    controls=[
+                        ft.IconButton(
+                            ft.icons.QUESTION_ANSWER, on_click=lambda e: on_navigation_change(2), icon_size=24),
+                        ft.Text("Тест", size=12,
                                 color=colors["Dark"]["text"]),
                     ],
                     alignment=ft.MainAxisAlignment.CENTER,
--- app/test.py
+++ app/test.py
@@ -1,5 +1,5 @@
 import flet as ft
-
+from config import BASE_DIR
 
 questions = [
     {
@@ -16,44 +16,75 @@ questions = [
     }
 ]
 
-test_screen = ft.Container(
-    content=ft.Column(
-        controls=[
-            ft.Text("Тест", size=24, weight=ft.FontWeight.BOLD, color="#FFDD2D"),
-            ft.Text("Вопрос 1", size=20,
-                    weight=ft.FontWeight.BOLD, color="#FFDD2D"),
-            ft.Text(questions[0]["question"], size=16,
-                    weight=ft.FontWeight.BOLD, color="#FFDD2D"),
-            ft.RadioGroup(
-                options=questions[0]["options"],
-                on_change=lambda e: print(e.control.value),
-            ),
-            ft.Text("Вопрос 2", size=20,
-                    weight=ft.FontWeight.BOLD, color="#FFDD2D"),
-            ft.Text(questions[1]["question"], size=16,
-                    weight=ft.FontWeight.BOLD, color="#FFDD2D"),
-            ft.RadioGroup(
-                options=questions[1]["options"],
-                on_change=lambda e: print(e.control.value),
-            ),
-            ft.Text("Вопрос 3", size=20,
-                    weight=ft.FontWeight.BOLD, color="#FFDD2D"),
-            ft.Text(questions[2]["question"], size=16,
-                    weight=ft.FontWeight.BOLD, color="#FFDD2D"),
-            ft.RadioGroup(
-                options=questions[2]["options"],
-                on_change=lambda e: print(e.control.value),
-            ),
-            ft.Container(
-                content=ft.Row(
-                    controls=[
-                        ft.Container(
-                            content=ft.Text(
-                                "Далее", size=16, weight=ft.FontWeight.BOLD, color="#FFDD2D"),
-                            alignment=ft.alignment.center,
-                            bgcolor="#313132",
-                            width=100,
-                            height=40,
-                            border_radius=10,
-                        )
-                    ],))]))
+controls = []
+
+for question in questions:
+    question_card = ft.Card(
+        content=ft.Row(
+            controls=[
+                ft.Container(
+                    content=ft.Column(
+                        controls=[
+                            ft.Text(
+                                question["question"], size=16, weight=ft.FontWeight.BOLD, color="#FFDD2D"),
+                            ft.RadioGroup(
+                                content=ft.Column(
+                                    controls=[
+                                        ft.Radio(value=option, label=option)
+                                        for option in question["options"]
+                                    ]
+                                ),
+                                on_change=lambda e: print(e.control.value)
+                            )
+                        ],
+                        alignment=ft.MainAxisAlignment.START,
+                        spacing=10,
+                    ),
+                    padding=10,
+                    expand=True
+                ),
+                ft.Container(
+                    content=ft.Image(
+                        src=f"{BASE_DIR}/static/question_image.png",
+                        fit=ft.ImageFit.COVER
+                    ),
+                    width=100,
+                    height=100,
+                    padding=10,
+                    clip_behavior=ft.ClipBehavior.ANTI_ALIAS
+                )
+            ],
+            alignment=ft.MainAxisAlignment.START,
+            spacing=10,
+        ),
+        elevation=5,
+        width=None,
+        height=None,
+        animate_size=True,
+    )
+
+    controls.append(question_card)
+
+list_view_content = ft.ListView(
+    controls=[
+        ft.Text("Тест", size=24, weight=ft.FontWeight.BOLD, color="#FFDD2D"),
+        *controls,
+        ft.Container(
+            content=ft.Row(
+                controls=[
+                    ft.Container(
+                        content=ft.Text(
+                            "Далее", size=16, weight=ft.FontWeight.BOLD, color="#FFDD2D"),
+                        alignment=ft.alignment.center,
+                        bgcolor="#313132",
+                        width=100,
+                        height=40,
+                        border_radius=10,
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
+        )
+    ],
+    expand=True
+)
